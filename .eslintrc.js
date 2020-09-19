2540f2723d0fd23014ce6e97a13055118dc8717e
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2020-09-19 16:36:43 +0800

    feat(git): 在commit前自动使用elint/style-lint去检查提交的代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,39 @@
-// .eslintrc.js
-module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-	"extends": ["eslint:recommended", 'plugin:react/recommended'],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": ["react"],
-    "rules": {
-        "indent": ["error", 4],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
-        "semi": ["error", "never"]
-    }
+module.exports =  {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'no-undef': 0 // off
+	}
 };

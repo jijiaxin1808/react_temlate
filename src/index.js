b23a38ac2d32fae028b32eafeeab6a0d46b03ea1
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2020-09-19 18:38:54 +0800

    refactor(项目结构): 更新ts为js,保留ts的配置

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-19 15:16:25
+ * @LastEditTime: 2020-09-19 16:05:42
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \react_temlate\src\index.js
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+

5c00c9357a70b8fa2129004bdcf3afedf0acc5db
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2020-09-20 19:51:03 +0800

    feat(less): 引入less并且设置theme

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,32 @@
-/* config-overrides.js */
+// /* config-overrides.js */
 const resolve = dir => require('path').resolve(__dirname, dir);
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra');
 
-module.exports = function override(config) {
-	//do stuff with the webpack config...
-	config.resolve = {
-		// 设置别名
-		alias: {
-			'@src': resolve('src'),// src 目录
-			'@comp': resolve('src/components'),
-			'@utils': resolve('src/utils'),
-			'@api': 'src/api'
-		}
-	};
-	return config;
-};
+module.exports = override(
+	// antd
+	fixBabelImports('import', {
+		libraryName: 'antd',
+		libraryDirectory: 'es',
+		style: true,
+	}),
+	// 这里配置less
+	addLessLoader({
+		strictMath: true,
+		noIeCompat: true,
+		modifyVars: {
+			'@primary-color': 'red', // 在这里配置主题
+		},
+		cssLoaderOptions: {},
+		cssModules: {
+			localIdentName: '[path][name]__[local]--[hash:base64:5]',
+		},
+		patterns: [resolve('../src/common.less')]
+	}),
+	// 这里配置目录的别名
+	addWebpackAlias({
+		'@src': resolve('src'),// src 目录
+		'@comp': resolve('src/components'),
+		'@utils': resolve('src/utils'),
+		'@api': 'src/api'
+	})
+);
--- src/modules/submodule1/app.js
+++ src/modules/submodule1/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './app.css';
-import Loading from '@c/Loading';
+import Loading from '@comp/Loading';
 
 
 function App() {

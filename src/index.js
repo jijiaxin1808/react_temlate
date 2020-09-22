ae3e5b62a25603d00efd6aae4d100c3eff1affe0
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2020-09-22 21:17:20 +0800

    feat(route): 增加普通的route配置

--- src/index.js
+++ src/index.js
@@ -4,12 +4,46 @@ import ReactDOM from 'react-dom';
 import App from '@src/modules/submodule1/app';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import {  BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import App2 from '@src/modules/submodule2';
+
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<Router>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about">About</Link>
+							</li>
+							<li>
+								<Link to="/users">Users</Link>
+							</li>
+						</ul>
+					</nav>
+
+					{/* A <Switch> looks through its children <Route>s and
+				renders the first one that matches the current URL. */}
+					<Switch>
+						<Route path="/about">
+							<App/>
+						</Route>
+						<Route>
+							<App2/>
+						</Route>
+						<Route path="/">
+							<App2/>
+						</Route>
+					</Switch>
+				</div>
+			</Router>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
 
---
+++ src/modules/submodule2/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+	return (
+		<p>module2</p>
+	);
+}
+
+export default App;
